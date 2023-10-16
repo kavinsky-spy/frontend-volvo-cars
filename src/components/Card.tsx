42c256f0aafbdcfc9b7214b46c5e5fb6526ab40a
kavinsky-spy <juliorapa@hotmail.com>
2023-10-16 01:05:51 -0300

    feat-wip: components hook

---
+++ src/components/Card.tsx
@@ -0,0 +1,13 @@
+interface CardProps {
+    id: string,
+    bodyType: string,
+    imageUrl: string,
+    modelName: string,
+    modelType: string
+}
+
+export function Card(props:CardProps) {
+    return(
+        <></>
+    )
+}
--- src/components/HelloWorld.tsx
+++ src/components/HelloWorld.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { Block, Button } from "vcc-ui";
+import { useCars } from "../hooks/useCars";
 
 export const HelloWorld: React.FC = () => {
+  const { cars } = useCars();
+
+  console.log(cars);
   return (
     <Block extend={{ padding: 20 }}>
       <Button>Click me!</Button>
